f05f57bcd2a19462012394dab883b12f6d40aedf
Daniel Yuan <daniel1yuan@gmail.com>
2017-02-25 12:31:10 -0600

    Finished user authentication backend and frontend

--- Persist/webapp/urls.py
+++ Persist/webapp/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^home/$', views.home, name='home'),
-  url(r'^login/$', views.login_user, name='login'),
-  url(r'^logout/$', views.logout_user, name='logout'),
+  url(r'^login/$', views.login_page, name='login_page'),
+  url(r'^_auth/$', views.is_logged_in, name='auth'),
+  url(r'^_login/$', views.login_user, name='login'),
+  url(r'^_logout/$', views.logout_user, name='logout'),
+  url(r'^_adduser/$', views.add_user, name='add_user'),
+  url(r'^_del_cur_user/$', views.del_cur_user, name='del_cur_user'),
+  url(r'^_deluser/$', views.del_user, name='deluser'),
 ]
