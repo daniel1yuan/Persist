1246ddc0bb26e41a1d87f386789de8d7e52ff5fb
Daniel Yuan <daniel1yuan@gmail.com>
2017-02-26 02:58:08 -0600

    Added dynamic loading

--- Persist/webapp/static/webapp/js/home.js
+++ Persist/webapp/static/webapp/js/home.js
@@ -1,9 +1,9 @@
 var test;
 
 //Given a habit object, return time remaining in seconds
-function timeSince(last_clicked){
+function timeSince(reference){
   var now = new Date();
-  var timeSince = now.getTime() - last_clicked.getTime();
+  var timeSince = now.getTime() - reference.getTime();
   return timeSince/1000;
 }
 
@@ -21,15 +21,38 @@ function sort_timeSince(habits){
   return sortedArray;
 }
 
-function loadHabits(habits){
-  for (var i in habits){
-    console.log(habits[i]);
+function adjustBars(barIDs, habits){
+  var daySeconds = 86400;
+  for (var i in barIDs){
+    var curBar= barIDs[i];
+    var percent = timeSince(habits[parseInt(i)].last_clicked)/daySeconds * 100;
+    $("#" + barIDs[i]).css("width", Math.round(percent).toString() +"%");
+    console.log(Math.round(percent).toString()+"%");
   }
+}
+
+function loadHabits(habits){
+  var parDiv = ".test";
+  var barIDs = {};
+  for (var i in habits){
+    var curHabit = habits[i];
+    var id = curHabit.id;
+    var name = curHabit.name;
+    var cost = curHabit.monetary_amount;
+    var streak = Math.round(timeSince(curHabit.start_date)/(3600*24));
+    $(parDiv).append("<div class=habitbar id=habit-"+id+"><div class=habitbartext><div class=habitbartextstreak><h1 id=streak-"+id+">"+streak+"</h1></div><div class=habitbartextname><h1 id=name-"+id+">"+name+"</h1></div><div class=habitbartextmoney><h1 id=cost-"+id+">$ "+cost+"</h1></div></div><div class=habitbartimeleftbackground> <div class=habitbartimeleftforeground id=timeleft-"+id+"></div> </div></div>");
+    console.log(habits[i]);
+    barIDs[id]= "timeleft-"+id;
+  }
+  return {"bar": barIDs};
 }
 
 $(document).ready(function(){
   get_all_habits(function(habits){
-    test = sort_timeSince(habits);
-    loadHabits(test);
+    habitDict = habits;
+    habits = sort_timeSince(habits);
+    Ids = loadHabits(habits);
+    console.log(Ids);
+    adjustBars(Ids.bar, habitDict);
   });
 });
